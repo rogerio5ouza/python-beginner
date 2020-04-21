3e29ecfe6c87bd95529659f8a2739da580f944f0
Rogério Souza <rogerio_dsouza@hotmail.com>
2020-04-21 16:31:17 -0300

    Tipos de Variáveis

---
+++ semana-1/variaveis.py
@@ -0,0 +1,16 @@
+# Tipos de Variáveis em Python 
+
+bool (-lean) True ou False
+int (-eger)  1, 2, 10, 5000
+float        3.5432, 9.8, 1.61803398875
+list         [ 'palavra', True, 1, 3.1415]
+dict         { 'prop':'nome', 'tem':True }
+tuple        ( 'palavra', True, 1, 3.1415)
+type         Tipo dos tipos
+
+
+
+the_world_is_flat = True
+if the_world_is_flat:
+    print("Be careful not to fall off!")
+
