49121312a9eb1a2e9dbf2d6875b3fbe558cb6ad8
Rogério Souza <rogerio_dsouza@hotmail.com>
2020-05-14 19:42:01 -0300

    add doc syntax

---
+++ semana-1/syntax.py
@@ -0,0 +1,38 @@
+# A sintaxe Python para a função print():
+
+print('Hello, World!')
+
+# Indentação Python
+
+'''
+Indentação em Python refere-se aos espaços no início de uma linha de código.
+
+Onde em outras linguagens de programação o recuo no código é apenas para legibilidade, em Python é muito importante.
+
+Python usa indentação para indicar um bloco de código.
+'''
+# Exemplo:
+
+if 5 > 2:
+    print('Cinco é maior que dois!')
+
+'''
+O número de espaços depende de você como programador, mas deve ser pelo menos um.
+'''
+# Exemplo:
+
+if 5 > 2:
+    print('Cinco é maior que dois!')
+if 5 > 2:
+    print('Cinco é maior que dois!')
+
+'''
+Você precisa usar o mesmo número de espaços no mesmo bloco de código, caso contrário, o Python apresentará um erro:
+'''
+# Exemplo:
+
+'''
+if 5 > 2:
+ print('Cinco é maior que dois!')       # Syntax Error
+    print('Cinco é maior que dois!')    # Syntax Error
+'''
