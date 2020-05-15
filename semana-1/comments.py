ed2d90ab3a74b63f5f26983b0558884e68e28929
Rogério Souza <rogerio_dsouza@hotmail.com>
2020-05-15 17:38:01 -0300

    add doc comments

---
+++ semana-1/comments.py
@@ -0,0 +1,39 @@
+'''
+Comentários em Python
+
+Comentários podem ser usados para explicar o código Python.
+Os comentários podem ser usados para tornar o código mais legível.
+Os comentários podem ser usados para impedir a execução ao testar o código.
+Os comentários começam com um # e o Python os ignorará:
+'''
+# Exemplo:
+
+# Isso é um comentário de uma linha
+print('Hello, World!')
+
+'''
+Os comentários podem ser colocados no final de uma linha, e o Python ignorará o restante da linha:
+'''
+print('Hello, World!') # Isso é um comentário
+
+'''
+Os comentários não precisam ser texto para explicar o código, também podem ser usados para impedir 
+que o Python execute o código:
+'''
+
+# print('Hello, World!')
+print('Olá, amigo!')
+
+'''
+Comentários de múltiplas linhas:
+'''
+
+"""
+
+This is a comment
+written is
+more than just one line
+"""
+
+print('Hello, World!')
+
