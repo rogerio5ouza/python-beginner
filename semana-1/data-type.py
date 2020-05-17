c8e4583fdcf29266d66706b5c5feceb0e3300d96
Rogério Souza <rogerio_dsouza@hotmail.com>
2020-05-17 18:18:33 -0300

    add data type
    Conceito dos diferentes Tipos de Dados

---
+++ semana-1/data-type.py
@@ -0,0 +1,24 @@
+'''
+Tipos de Dados (Data Types)
+
+Em programação, Data Types é um importante conceito.
+Variáveis podem armazenar dados de diferentes tipos, e diferentes tipos podem fazer diferentes coisas.
+Python possui os seguintes Data Types embarcaddos por default, nessas categorias:
+'''
+
+# Text Type:          str
+# Numeric Types:      int, float, complex
+# Sequence Types:     list, tuple, range
+# Mapping Type:       dict
+# Set Types:          set, frozenset
+# Boolean Type:       bool
+# Binary Types:       bytes, bytearray, memoryview
+
+'''
+Obtendo os Data Types
+
+Podemos obter o Data Type de qualquer objeto usando a função type(): 
+'''
+
+x = 5
+print(type(x)) # <class 'int'>
