2c97c2c90e954632632093b7ad90341a8b7fe260
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-05-25 21:12:18 -0300

    Create booleans.py
    O tipo Boolean representa um de dois valores: True ou False.
    
    Quando você compara dois valores, a expressão é avaliada e o Python retorna a resposta booleana.

---
+++ semana-2/booleans.py
@@ -0,0 +1,14 @@
+'''
+Python Booleans
+
+Booleans representa um de dois valores: True ou False.
+
+Valores Booleanos
+
+Quando você compara dois valores, a expressão é avaliada e o Python retorna a resposta Booleana.
+'''
+# Exemplo:
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
