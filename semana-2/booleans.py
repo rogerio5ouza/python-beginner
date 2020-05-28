5758ce2ac3777c1df3fdd82d0e5fe0b844f9174b
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-05-28 07:57:40 -0300

    Update booleans.py
    Exemplos de uso da function bool().
    A function bool() permite avaliar qualquer valor e retornar True ou False.

--- semana-2/booleans.py
+++ semana-2/booleans.py
@@ -12,3 +12,36 @@ Quando você compara dois valores, a expressão é avaliada e o Python retorna a
 print(10 > 9)
 print(10 == 9)
 print(10 < 9)
+
+'''
+Quando executmaos uma condição em uma instrução IF, o Python retorna True ou False.
+'''
+# Exemplo:
+
+a = 200
+b = 33
+
+if b > a:
+    print('b é maior do que a')
+else:
+    print('b não é maior do que a')
+
+
+'''
+Evaluate Values and Variables (Avaliar valores e variáveis)
+
+A função bool() permite avaliar qualquer valor e retornar True ou False.
+'''
+# Exemplo:
+# Avalia uma string e um número:
+
+print(bool('Hello'))
+print(bool(15))
+
+# Avalia duas variáveis:
+
+x = 'Hello'
+y = 15
+
+print(bool(x))
+print(bool(y))
