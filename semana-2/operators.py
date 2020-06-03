9a2ec080fdf5fcd95a9d56b701ea3ceb0e4b0a12
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-06-03 07:28:24 -0300

    Create operators.py
    Uso dos diferentes tipos de operadores em Python, para execução de operações em variáveis e valores..

---
+++ semana-2/operators.py
@@ -0,0 +1,54 @@
+'''
+Operators (Operadores)
+
+Operadores em Python são usados para executar operações em variáveis e valores.
+Python divide os Operadores nos seguintes grupos:
+'''
+
+# Arithmetic operators (Operadores aritméticos)
+# Assignment operators (Operadores de atribuição)
+# Comparison operators (Operadores de comparação)
+# Logical operators    (Operadores lógicos)
+# Identity operators   (Operadores de identidade)
+# Membership operators (Operadores de associação)
+# Bitwise operators    (Operadores bit a bit)
+
+'''
+Arithmetic operators 
+
+São usados com valores numéricos para executar operações matemáticas comuns.
+'''
+# Operador Addition (+)
+x = 5
+y = 3
+print(x + y)
+
+# Operador Subtraction (-)
+x = 5
+y = 3
+print(x - y)
+
+# Operador Multiplication (*)
+x = 5
+y = 3
+print(x * y)
+
+# Operador Division (/)
+x = 12
+y = 3
+print(x / y)
+
+# Operador Modulus (%)
+x = 5
+y = 2
+print(x % y)
+
+# Operador Exponentiation (**)
+x = 2
+y = 5
+print(x ** y)  # igual a 2*2*2*2*2
+
+# Operador Floor Division (//)
+x = 15
+y = 2
+print(x // y)  # a divisão // arredonda o resultado para o número inteiro mais próximo
