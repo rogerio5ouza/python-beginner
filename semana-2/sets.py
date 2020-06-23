b712cdb82300d2c8d5bb855d509627d74b6552f9
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-06-23 10:02:01 -0300

    Update sets.py
    Acessando itens de um conjunto com FOR IN.

--- semana-2/sets.py
+++ semana-2/sets.py
@@ -10,3 +10,28 @@ set_de_animais_felinos = {
 }
 
 print(set_de_animais_felinos)
+
+print(type(set_de_animais_felinos))
+
+'''
+  Acesso de itens (Access Items)
+
+  Não podemos acessar itens de um conjunto consultando seu índice, pois os conjuntos são desordenados e seus itens não possuem índice.
+  Mas podemos percorrer os itens do conjunto usando um loop for ou perguntar se um valor específico está presente em um conjunto, usando a in keyword.
+'''
+# Exemplo:
+
+set_de_animais_felinos = {
+    'Tigre', 'Leao', 'Lince', 'Onca-pintada', 'Leopardo-das-neves', 'Guepardo'
+}
+
+for x in set_de_animais_felinos:
+    print(x)
+
+# Verifica se 'Guepardo' está presente no conjunto:
+
+set_de_animais_felinos = {
+    'Tigre', 'Leao', 'Lince', 'Onca-pintada', 'Leopardo-das-neves', 'Guepardo'
+}
+
+print('Guepardo' in set_de_animais_felinos)
