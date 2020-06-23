ec18fc0e26494c69429b89a54a385ef63b518a0a
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-06-23 09:44:46 -0300

    Create sets.py
    Uso de Sets (conjuntos) em Python.

---
+++ semana-2/sets.py
@@ -0,0 +1,12 @@
+'''
+SET (Conjunto)
+
+Um Set é uma coleção de itens não ordenada e nem indexada. Em Python Sets são escritos entre colchetes {}.
+'''
+# Exemplo:
+
+set_de_animais_felinos = {
+    'Tigre', 'Leao', 'Lince', 'Onca-pintada', 'Leopardo-das-neves', 'Guepardo'
+}
+
+print(set_de_animais_felinos)
