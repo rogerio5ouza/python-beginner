e01c84ba6add09796995f1d7b5c1a75e7698bd73
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-07-08 08:18:54 -0300

    Create for_loops.py
    Implementação do loop for.

---
+++ semana-3/for_loops.py
@@ -0,0 +1,10 @@
+'''
+For Loops
+
+Um loop for é usado para iterar sobre uma sequência (percorre uma lista, tupla, dicionário, conjunto de string).
+'''
+# Exemplo:
+
+frutas = ['amora', 'banana', 'cereja', 'goiaba', 'morango', 'uva']
+for f in frutas:
+    print(f)
