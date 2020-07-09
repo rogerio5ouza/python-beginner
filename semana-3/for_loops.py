1501cc1e8122e9d983b01618bab7b6570d5ebd8b
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-07-09 09:04:04 -0300

    Update for_loops.py
    Implementação do loop For em uma String.

--- semana-3/for_loops.py
+++ semana-3/for_loops.py
@@ -8,3 +8,14 @@ Um loop for é usado para iterar sobre uma sequência (percorre uma lista, tupla
 frutas = ['amora', 'banana', 'cereja', 'goiaba', 'morango', 'uva']
 for f in frutas:
     print(f)
+
+'''
+Loop atrevés de uma String
+
+Mesmo as Strings são objetos iteráveis, elas contém uma sequência de caracteres.
+'''
+
+# Exemplo:
+
+for f in 'banana':
+    print(f)
