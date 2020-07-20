851b05cc00aa6b168b1c0ddd3bdb8ad8f26768af
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-07-20 09:43:33 -0300

    Create arrays.py
    Implementação de Arrays.

---
+++ semana-3/arrays.py
@@ -0,0 +1,11 @@
+'''
+Arrays
+
+Nota: O Python não possui suporte nativo para matrizes, mas as listas do Python podem ser usadas como Arrays.
+No entanto para trabalhar com matrizes no Python, precisamos importar uma biblioteca, como a NumPy.
+
+Arrays são usados para armazenar múltiplos valores em uma variável.
+'''
+# Exemplo:
+carros = ['Fiat', 'Ford', 'Volkswagen', 'Toyota']
+print(carros)
