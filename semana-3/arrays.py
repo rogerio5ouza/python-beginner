968c28c51ca4180a53ca258d5b39411338e0b8e0
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-07-21 08:54:14 -0300

    Update arrays.py
    Definição do termo Array.

--- semana-3/arrays.py
+++ semana-3/arrays.py
@@ -1,6 +1,12 @@
 '''
 Arrays
 
+O que é um Array?
+
+Um Array é uma variável especial, que pode conter mais de um valor.
+
+Um Array pode conter muitos valores com um único nome e podemos acessar esses valores consultando um número do índice.
+
 Nota: O Python não possui suporte nativo para matrizes, mas as listas do Python podem ser usadas como Arrays.
 No entanto para trabalhar com matrizes no Python, precisamos importar uma biblioteca, como a NumPy.
 
