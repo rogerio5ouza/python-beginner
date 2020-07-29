ffbcf86c6c02e8a9a0490fd43e49112fd6141f4b
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-07-29 09:21:22 -0300

    Create classes_objects.py
    Implementação de uma classe e criação de um objeto.

---
+++ semana-3/classes_objects.py
@@ -0,0 +1,19 @@
+'''
+Classes and Objects (Classes e Objetos)
+
+Quase tudo no Python é um objeto, com suas propriedades e métodos.
+Uma classe é como um construtor de objetos ou um modelo para criar objetos.
+'''
+# Exemplo:
+
+
+class MinhaClasseNome:
+    nome = 'roger'
+
+
+print(MinhaClasseNome)
+
+# Criação do objeto nome1 e impressão do valor nome:
+
+nome1 = MinhaClasseNome()
+print(nome1.nome)
