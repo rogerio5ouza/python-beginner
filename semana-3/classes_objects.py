81adb734e8a0b61820a280a52b3aec8e4f181597
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-07-30 09:15:02 -0300

    Update classes_objects.py
    Implementação da function __init__().

--- semana-3/classes_objects.py
+++ semana-3/classes_objects.py
@@ -17,3 +17,26 @@ print(MinhaClasseNome)
 
 nome1 = MinhaClasseNome()
 print(nome1.nome)
+
+'''
+The __init__() Function 
+
+Para entender o significado das classes, precisamos entender a função interna __init__().
+
+Todas as classes têm uma função chamada __init__(), que é sempre executada quando a classe está sendo iniciada.
+
+Usamos a função __init__() para atribuir valores às propriedades do objeto ou outras operações necessárias quando o objeto estiver sendo criado.
+'''
+# Exemplo:
+
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+p1 = Pessoa('Robert', 32)
+
+print(p1.nome)
+print(p1.idade)
