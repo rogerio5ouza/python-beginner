db3322979e917aebb69af0f19473dcf8b7828742
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-08-12 09:13:34 -0300

    Update iterators.py
    Implementação dos métodos:
    
    - __iter__();
    -__next__().

--- semana-4/iterators.py
+++ semana-4/iterators.py
@@ -1,3 +1,35 @@
 '''
 Python Iterators
+
+Um Iterathor é um objeto que contém um número contável de valores.
+Que pode ser iterado, o que significa que podemos percorrer todos os valores.
+Tecnicamente, em Python, um Iterathor é um objeto que implementa o protocolo do Iterathor, que consiste nos métodos __iter__() e __next__().
+
+Iterathor vs Iterable
+
+Lists, tuples, dictionaries e sets são todos objetos iteráveis. Eles são contêineres iteraveis dos quais podemos obter um Iterathor.
+Todos esses objetos possuem um método iter() que é usado para obter um Iterathor.
 '''
+# Exemplo:
+# Retorna um Iterathor de uma tupla e, imprime cada um de seu valor:
+minha_tuple_frutas = ('apple', 'banana', 'cherry', 'orange', 'melon')
+meu_iterathor = iter(minha_tuple_frutas)
+
+print(next(meu_iterathor))
+print(next(meu_iterathor))
+print(next(meu_iterathor))
+print(next(meu_iterathor))
+print(next(meu_iterathor))
+
+# Até mesmo Strings são objetos iráveis e podem retornar um Iterathor.
+# Exemplo:
+
+minha_string = 'banana'
+meu_iterathor_2 = iter(minha_string)
+
+print(next(meu_iterathor_2))
+print(next(meu_iterathor_2))
+print(next(meu_iterathor_2))
+print(next(meu_iterathor_2))
+print(next(meu_iterathor_2))
+print(next(meu_iterathor_2))
