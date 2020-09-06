768f56808fdc59ff73917c804610ef4ed261c342
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-09-06 10:32:55 -0300

    Create dates.py
    Implementação do módulo datetime.

---
+++ semana-4/dates.py
@@ -0,0 +1,13 @@
+'''
+Datas
+
+Uma data em Python não é um tipo de dado próprio, mas podemos importar um módulo denominado 'datetime' para 
+trabalhar com datas como objetos de data.
+'''
+# Exemplo
+# Importa o módulo 'datetime' e exibe a data atual:
+
+import datetime
+
+hora_atual = datetime.datetime.now()
+print(hora_atual)
