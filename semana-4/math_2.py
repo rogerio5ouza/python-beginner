a3b9d20f071dcf54f500231a2ec3724ac2b495a7
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-09-15 22:22:51 -0300

    Create math_2.py
    Implementação do módulo Math.

---
+++ semana-4/math_2.py
@@ -0,0 +1,28 @@
+'''
+O módulo Math
+
+Python possui um módulo integrado chamado Math, que estende uma lista de funções matemáticas.
+
+Para usá-lo, devemos importá-lo.
+
+Depois de importá-lo, podemos começar a usar os métodos e constantes.
+'''
+
+# O método math.sqrt() retorna a raiz quadrada de um número:
+
+import math
+
+numero_quadrado = math.sqrt(49)
+print(numero_quadrado)
+
+'''
+O método math.ceil() arredonda um número para cima, para seu inteiro mais próximo, e o método math.floor() 
+arredonda um número para baixo, para seu inteiro mais próximo e retorna o resultado.
+'''
+# Exemplo:
+
+arredonda_pra_cima = math.ceil(1.4)
+arredonda_pra_baixo = math.floor(1.4)
+
+print(arredonda_pra_cima)
+print(arredonda_pra_baixo)
