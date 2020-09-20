efbda75073c185fbe16cc29066cb155984dceb26
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-09-20 17:57:58 -0300

    Create convert_python_objects.py
    Convert Python objects.

---
+++ semana-4/convert_python_objects.py
@@ -0,0 +1,24 @@
+'''
+Podemos converter objetos de tipos em Python em Strings JSON:
+
+- dict
+- list
+- tuple
+- string
+- int
+- float
+- True
+- False
+- None
+
+'''
+import json
+
+print(json.dumps({"nome": "Joao", "idade": 30}))
+print(json.dumps(("maca", "bananas")))
+print(json.dumps("ola"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
